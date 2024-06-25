b55f6d43bc5c983cde1e6669f18ac4c3330a08f3
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-06-25 21:58:23 +0300

    Add project files.

---
+++ AjaxProjesi/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProjesi.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=AYASINB\\AYASINB;initial Catalog=AjaxProjesi;integrated Security=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
